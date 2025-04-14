45d0fb85008dddcc6b021ed3b8f5fb2262b22656
KamSaf <bopaf42@gmail.com>
2025-04-14 13:55:13 +0200

    URAN-8 Add can_load, load_cargo and unload_cargo Truck class methods

--- src/truck.py
+++ src/truck.py
@@ -20,8 +20,42 @@ class Truck:
         self.start = start
         self.route = route
 
-    def can_load(self, weight: int) -> bool:
-        return weight <= self.space_left
+    def can_load(self, type: str, point: Point) -> bool:
+        weight = point.cargo[type]
+        if weight < 0 and (
+            type == "pomarańcze"
+            and self.cargo["uran"] != 0
+            or type == "uran"
+            and self.cargo["pomarańcze"] != 0
+        ):
+            return False  # nie można pomarańczy z uranem
+
+        if weight > 0:
+            # czy jedziemy tylko do punktów, które obsłużymy w 100%?
+            return self.cargo[type] > 0
+        elif weight < 0:
+            return self.space_left > 0
+        else:
+            return True
+
+    def load_cargo(self, type: str, point: Point) -> None:
+        weight = point.cargo[type]
+        if weight > 0:
+            cargo_to_load = weight if weight <= self.space_left else self.space_left
+            self.cargo[type] -= cargo_to_load
+            self.space_left += cargo_to_load
+            point.cargo[type] -= cargo_to_load
+        elif weight < 0:
+            cargo_to_load = (
+                weight if abs(weight) <= self.space_left else -self.space_left
+            )
+            self.cargo[type] -= cargo_to_load
+            self.space_left += cargo_to_load
+            point.cargo[type] -= cargo_to_load
+
+    def unload_cargo(self) -> None:
+        self.space_left = self.cargo
+        self.cargo = {key: 0 for key, _ in self.cargo.items()}
 
     def run(self):
         if not self.start or not self.route:
