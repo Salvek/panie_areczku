c0c2a6cb496e2182fcb87edfa02096d59bfd325c
KamSaf <bopaf42@gmail.com>
2025-04-14 10:10:32 +0200

    URAN-8 Add cargo and space_left attributes to Truck class, uncomment can_load function

--- src/truck.py
+++ src/truck.py
@@ -3,12 +3,14 @@ from typing import List
 from point import Point
 from util.move_truck import move_truck_along_route
 
+
 class Truck:
     def __init__(self, name: str, capacity: int):
         self.id = uuid.uuid4()
         self.name = name
         self.capacity = capacity
-        # self.space_left = capacity # TODO node do implementacji załadunku
+        self.cargo = {"tuna": 0, "oranges": 0, "uranium": 0}
+        self.space_left = capacity - sum(self.cargo.values())
         self.route: List[Point] = []
         self.start: Point = None
         self.total_distance = 0
@@ -18,13 +20,12 @@ class Truck:
         self.start = start
         self.route = route
 
-    # TODO node do implementacji załadunku
-    # def can_load(self, weight: int) -> bool:
-    #     return weight <= self.space_left
+    def can_load(self, weight: int) -> bool:
+        return weight <= self.space_left
 
     def run(self):
         if not self.start or not self.route:
             raise ValueError(f"🚫 {self.name} nie ma przypisanej trasy.")
         print(f"\n{self.name} rusza w trasę (ładowność: {self.capacity}kg)")
         self.result = move_truck_along_route(self.start, self.route)
-        self.total_distance = self.result['total_distance']
+        self.total_distance = self.result["total_distance"]
